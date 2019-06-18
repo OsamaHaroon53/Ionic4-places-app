028021a8f6af14a005cdd891d79c5b2e423a1789
OsamaHaroon53 <sonijaosama@gmail.com>
2019-06-18 15:42:01 +0500

    final commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsAPIKey: 'AIzaSyB8pf6ZdFQj5qw7rc_HSGrhUwQKfIe9ICw',
-  firebaseApiKey: 'AIzaSyAsIvYPid8xt7qGhxolXguMlq_woka-5LU'
+  googleMapsAPIKey: '',
+  firebaseApiKey: ''
 };
